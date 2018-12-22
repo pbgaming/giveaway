abbb619332110cf83b9c7dd31326ceb3365fb097
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-22 22:23:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dream | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
+client.user.setGame(`Epic Shop`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
